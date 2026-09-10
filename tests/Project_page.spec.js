e0cdf8ad45fbc8c2a408dbfd750b42fbd2ee177c
Sandesh <sandeshchy2059@gmail.com>
2026-09-10 13:17:00 +0545

    remove a project added, edit a project added

--- tests/Project_page.spec.js
+++ tests/Project_page.spec.js
@@ -48,3 +48,36 @@ test('Create or post a project', async ({page}) => {
     await page.getByRole('button', { name: 'Post Project' }).click();
     await expect(page.getByRole('heading', { name: 'B villa' })).toBeVisible();
 });
+
+test('remove/delete a project', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'I already have an account' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('jack16@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Sign In →' }).click();
+    await page.getByRole('link', { name: 'Projects', exact: true }).click();
+    await page.getByRole('link', { name: 'B villa' }).click();
+    await page.getByRole('button', { name: 'Delete' }).click();
+    await page.getByRole('button', { name: 'Yes' }).click();
+    await expect(page.getByRole('heading', { name: 'B Villa' })).not.toBeVisible();
+});
+
+test('edit a project', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'I already have an account' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('jack16@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Sign In →' }).click();
+    await page.getByRole('link', { name: 'Projects', exact: true }).click();
+    await page.getByRole('link', { name: 'B villa' }).click();
+    await page.getByRole('button', { name: 'Edit' }).click();
+    await page.getByRole('textbox', { name: 'Modern Residential Villa' }).click();
+    await page.getByRole('textbox', { name: 'Modern Residential Villa' }).fill('C villa');
+    await page.getByRole('button', { name: 'Save' }).click();
+    await expect(page.getByRole('heading', { name: 'C villa' })).toBeVisible();
+});
+
