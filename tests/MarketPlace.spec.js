23da9893e7ea3a425b09a797bff0360c20b254e2
Sandesh <sandeshchy2059@gmail.com>
2026-09-10 10:37:40 +0545

    test case automation for search changing filter setting of location and minimum rating

--- tests/MarketPlace.spec.js
+++ tests/MarketPlace.spec.js
@@ -370,3 +370,23 @@ test('verify search results after changing filer settings to only verified', asy
   await expect(page.getByRole('heading', { name: 'Jack Sparrow' })).not.toBeVisible();
 });
 
+test('Verify search result after changing filter settings of location and minimum rating', async ({page}) => {
+  await page.goto('https://dev6.yigserver.com:3000/');
+  await page.getByRole('link', { name: 'I already have an account' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('jack16@example.com');  
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+  await page.getByRole('button', { name: 'Sign In →' }).click();
+  await page.getByRole('link', { name: 'Market', exact: true }).click();
+  await page.getByRole('button').filter({ hasText: /^$/ }).click();
+  await page.getByRole('dialog', { name: 'Filters' }).click();
+  await page.getByRole('combobox').first().selectOption('Kathmandu');
+  await page.getByRole('combobox').nth(1).selectOption('4');
+  await page.getByRole('button', { name: 'Apply' }).click();
+  await expect(page.getByRole('button', { name: '2' })).toBeVisible();
+  await expect(page.getByText('Kathmandu').first()).toBeVisible();
+  await expect(page.getByText('4.0') ).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Jack Sparrow' })).not.toBeVisible();
+
+});
