656d0eb9ae6faafd101bcae884ecc394ea28bfd8
Sandesh <sandeshchy2059@gmail.com>
2026-09-10 09:20:08 +0545

    test case automation for filter function in marketplace

--- tests/MarketPlace.spec.js
+++ tests/MarketPlace.spec.js
@@ -305,3 +305,16 @@ test('verify supplier name or skill in other sections', async ({page}) => {
   await page.getByRole('textbox', { name: 'Search by name or skill…' }).fill('Himalayan Cement & Steel Depot');
   await expect(page.getByText('No results')).toBeVisible();
 });
+
+test('Verify working of filter button', async ({page}) => {
+  await page.goto('https://dev6.yigserver.com:3000/');
+  await page.getByRole('link', { name: 'I already have an account' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('jack16@example.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+  await page.getByRole('button', { name: 'Sign In →' }).click();
+  await page.getByRole('link', { name: 'Market', exact: true }).click();
+  await page.getByRole('button').filter({ hasText: /^$/ }).click();
+  await expect(page.getByRole('dialog', { name: 'Filters' })).toBeVisible();
+});
