e9dbc6f743dd707d248e1f63aaf6104f7dc37469
Sandesh <sandeshchy2059@gmail.com>
2026-09-11 16:33:24 +0545

    verify search using existing  project name but different letter case

--- tests/leads_page.spec.js
+++ tests/leads_page.spec.js
@@ -56,3 +56,18 @@ test('verify search with leading or trailing spaces', async ({page}) => {
   await page.getByRole('textbox', { name: 'Search projects…' }).fill('   sano ghar   ');
   await expect(page.getByRole('link', { name: 'active sano ghar NPR 6,554 –' })).toBeVisible();
 });
+
+test('verify search with different letter case', async ({page}) => {
+  await page.goto('https://dev6.yigserver.com:3000/');
+  await page.getByRole('link', { name: 'I already have an account' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('jack21@example.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+  await page.getByRole('button', { name: 'Sign In →' }).click();
+  await page.getByRole('link', { name: 'Leads' }).click();
+  await page.getByRole('heading', { name: 'Leads' }).click();
+  await page.getByRole('textbox', { name: 'Search projects…' }).click();
+  await page.getByRole('textbox', { name: 'Search projects…' }).fill('Sano Ghar');
+  await expect(page.getByRole('link', { name: 'active sano ghar NPR 6,554 –' })).toBeVisible();
+});
