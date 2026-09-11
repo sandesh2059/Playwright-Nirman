5fdc373c0b69173395862e13ece25e0f94e094be
Sandesh <sandeshchy2059@gmail.com>
2026-09-11 17:04:10 +0545

    verify my proposal section

--- tests/leads_page.spec.js
+++ tests/leads_page.spec.js
@@ -124,3 +124,16 @@ test('verify clicking on a project', async ({page}) => {
   
   await expect(page).toHaveURL('https://dev6.yigserver.com:3000/projects/54');
 });
+
+test('verify my proposal section', async ({page}) => {
+  await page.goto('https://dev6.yigserver.com:3000/');
+  await page.getByRole('link', { name: 'I already have an account' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('jack21@example.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+  await page.getByRole('button', { name: 'Sign In →' }).click();
+  await page.getByRole('link', { name: 'Leads' }).click();
+  await page.getByRole('button', { name: 'My Proposals' }).click();
+  await expect(page.getByRole('button', { name: 'My Proposals' })).toHaveClass(/border-primary/);
+});
