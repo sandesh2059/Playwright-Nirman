83804adf37f714ed10bbe66cd565bffed4697f18
Sandesh <sandeshchy2059@gmail.com>
2026-09-11 16:38:01 +0545

    verify search using filter setting set to most recent

--- tests/leads_page.spec.js
+++ tests/leads_page.spec.js
@@ -71,3 +71,16 @@ test('verify search with different letter case', async ({page}) => {
   await page.getByRole('textbox', { name: 'Search projects…' }).fill('Sano Ghar');
   await expect(page.getByRole('link', { name: 'active sano ghar NPR 6,554 –' })).toBeVisible();
 });
+
+test('verify search with filter set to most recent', async ({page}) => {
+  await page.goto('https://dev6.yigserver.com:3000/');
+  await page.getByRole('link', { name: 'I already have an account' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('jack21@example.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+  await page.getByRole('button', { name: 'Sign In →' }).click();
+  await page.getByRole('link', { name: 'Leads' }).click();
+  await page.getByRole('combobox').nth(1).selectOption('recent');
+  await expect(page.getByRole('combobox').nth(1)).toHaveValue('recent');
+});
