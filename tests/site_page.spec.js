132e50d524210079088147c8d1fc80ee1d05e70e
Sandesh <sandeshchy2059@gmail.com>
2026-09-11 12:16:02 +0545

    Creating new sites by leaving all fields empty

--- tests/site_page.spec.js
+++ tests/site_page.spec.js
@@ -37,3 +37,17 @@ test('Creating new sites by filling all the fields', async ({page}) => {
     await page.getByRole('navigation').getByRole('link', { name: 'Sites' }).click();
     await expect(page.getByRole('link', { name:  `${siteName} Jack` })).toBeVisible();
 });
+
+test('verify creating new sites by leaving all fields empty', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'I already have an account' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('jack21@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Sign In →' }).click();
+    await page.getByRole('link', { name: 'Sites' }).click();
+    await page.getByRole('link', { name: 'New site' }).first().click();
+    await page.getByRole('button', { name: 'Create site' }).click();
+    await expect(page.getByRole('textbox', { name: 'Villa Extension' })).toHaveAttribute('required', '');
+});
