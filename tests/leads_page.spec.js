cb9e8f973049357dba0f5cc3e09f8ad6c31931ae
Sandesh <sandeshchy2059@gmail.com>
2026-09-11 16:43:54 +0545

    verify search using filter setting set to highest budget

--- tests/leads_page.spec.js
+++ tests/leads_page.spec.js
@@ -84,3 +84,16 @@ test('verify search with filter set to most recent', async ({page}) => {
   await page.getByRole('combobox').nth(1).selectOption('recent');
   await expect(page.getByRole('combobox').nth(1)).toHaveValue('recent');
 });
+
+test('verify search with filter setting set to highest budgets', async ({page}) => {
+  await page.goto('https://dev6.yigserver.com:3000/');
+  await page.getByRole('link', { name: 'I already have an account' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('jack21@example.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+  await page.getByRole('button', { name: 'Sign In →' }).click();
+  await page.getByRole('link', { name: 'Leads' }).click();
+  await page.getByRole('combobox').nth(1).selectOption('budget');
+  await expect(page.getByRole('combobox').nth(1)).toHaveValue('budget');
+});
