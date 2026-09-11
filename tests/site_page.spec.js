b1e50f8f18553b8d9bc57312a373721efc50d6cd
Sandesh <sandeshchy2059@gmail.com>
2026-09-11 12:31:19 +0545

    Creating new sites by only filling sitename field

--- tests/site_page.spec.js
+++ tests/site_page.spec.js
@@ -51,3 +51,22 @@ test('verify creating new sites by leaving all fields empty', async ({page}) => 
     await page.getByRole('button', { name: 'Create site' }).click();
     await expect(page.getByRole('textbox', { name: 'Villa Extension' })).toHaveAttribute('required', '');
 });
+
+test('verify creating new sites by filling only the site name field', async ({page}) => {
+    const siteName = `Sano ghar upgrade ${Date.now()}`;
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'I already have an account' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('jack21@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Sign In →' }).click();
+    await page.getByRole('link', { name: 'Sites' }).click();
+    await page.getByRole('link', { name: 'New site' }).first().click();
+    await page.getByRole('textbox', { name: 'Villa Extension' }).click();
+    await page.getByRole('textbox', { name: 'Villa Extension' }).fill(siteName);
+    await page.getByRole('button', { name: 'Create site' }).click();
+    await expect(page.getByRole('heading', { name: siteName})).toBeVisible();
+    await page.getByRole('navigation').getByRole('link', { name: 'Sites' }).click();
+    await expect(page.getByRole('link', { name:  siteName })).toBeVisible();
+});
