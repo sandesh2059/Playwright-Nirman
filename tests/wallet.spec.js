820d755a264229de2f9fff5d5e92ba8c061cc93b
Sandesh <sandeshchy2059@gmail.com>
2026-09-14 15:48:15 +0545

    verify working of wallet balance

---
+++ tests/wallet.spec.js
@@ -0,0 +1,29 @@
+import {test, expect} from '@playwright/test';
+
+test('verify working of wallet button in navigation bar', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'I already have an account' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('cement@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Sign In →' }).click();
+    await page.getByRole('link', { name: 'Wallet' }).click();
+    await expect(page).toHaveURL('https://dev6.yigserver.com:3000/wallet');
+});
+
+test('verify the correct wallet balance', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    const email = `jack${Date.now()}@example.com`;
+    await page.getByRole('link', { name: 'Create account' }).click();
+    await page.getByRole('textbox', { name: 'Full Name' }).click();
+    await page.getByRole('textbox', { name: 'Full Name' }).fill('jack sparrow');
+    await page.getByRole('textbox', { name: 'Email Address' }).click();
+    await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Create Account →' }).click();
+    await page.getByRole('button', { name: 'Skip for now' }).click();
+    await page.getByRole('navigation').getByRole('link', { name: 'Wallet' }).click();
+    await expect(page.getByText('Token balance100tokensTokens')).toBeVisible();
+    });
