9b3c3cf1a50b26e2466119fcb149ab564763af94
Sandesh <sandeshchy2059@gmail.com>
2026-09-14 16:41:25 +0545

    verify working buy token func

--- tests/wallet.spec.js
+++ tests/wallet.spec.js
@@ -27,3 +27,54 @@ test('verify the correct wallet balance', async ({page}) => {
     await page.getByRole('navigation').getByRole('link', { name: 'Wallet' }).click();
     await expect(page.getByText('Token balance100tokensTokens')).toBeVisible();
     });
+
+test('verify buy token for basic package', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    const email = `jack${Date.now()}@example.com`;
+    await page.getByRole('link', { name: 'Create account' }).click();
+    await page.getByRole('textbox', { name: 'Full Name' }).click();
+    await page.getByRole('textbox', { name: 'Full Name' }).fill('jack sparrow');
+    await page.getByRole('textbox', { name: 'Email Address' }).click();
+    await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Create Account →' }).click();
+    await page.getByRole('button', { name: 'Skip for now' }).click();
+    await page.getByRole('navigation').getByRole('link', { name: 'Wallet' }).click();
+    await page.getByRole('button', { name: 'Buy now' }).first().click();
+    await expect(page.getByText('Token balance600tokensTokens')).toBeVisible();
+});
+
+test('verify buy token for standard package', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    const email = `jack${Date.now()}@example.com`;
+    await page.getByRole('link', { name: 'Create account' }).click();
+    await page.getByRole('textbox', { name: 'Full Name' }).click();
+    await page.getByRole('textbox', { name: 'Full Name' }).fill('jack sparrow');
+    await page.getByRole('textbox', { name: 'Email Address' }).click();
+    await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Create Account →' }).click();
+    await page.getByRole('button', { name: 'Skip for now' }).click();
+    await page.getByRole('navigation').getByRole('link', { name: 'Wallet' }).click();
+    await page.getByRole('button', { name: 'Buy now' }).nth(1).click();
+    await expect(page.getByRole('link', { name: '2,100' })).toBeVisible();
+});
+
+test ('verify buy token for premium package', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    const email = `jack${Date.now()}@example.com`;
+    await page.getByRole('link', { name: 'Create account' }).click();
+    await page.getByRole('textbox', { name: 'Full Name' }).click();
+    await page.getByRole('textbox', { name: 'Full Name' }).fill('jack sparrow');
+    await page.getByRole('textbox', { name: 'Email Address' }).click();
+    await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Create Account →' }).click();
+    await page.getByRole('button', { name: 'Skip for now' }).click();
+    await page.getByRole('navigation').getByRole('link', { name: 'Wallet' }).click();
+    await page.getByRole('button', { name: 'Buy now' }).nth(2).click();
+    await expect(page.getByRole('link', { name: '5,100' })).toBeVisible();
+});
