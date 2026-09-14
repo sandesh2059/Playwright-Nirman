09cf818cdd38ab95b37a12f328b71d6c96cbad47
Sandesh <sandeshchy2059@gmail.com>
2026-09-14 15:48:04 +0545

    verify working of catalogue button/page

---
+++ tests/catalogue.spec.js
@@ -0,0 +1,13 @@
+import {test, expect} from '@playwright/test';
+
+test('verify working of catalogue button in navigation bar', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'I already have an account' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('cement@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Sign In →' }).click();
+    await page.getByRole('link', { name: 'Catalogue' }).click();
+    await expect(page).toHaveURL('https://dev6.yigserver.com:3000/catalogue');
+});
