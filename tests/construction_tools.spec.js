dc6205fa199947f38ce030d9f56157fe4c5b53c3
Sandesh <sandeshchy2059@gmail.com>
2026-09-14 11:33:31 +0545

    verify working of concrete calculator by not filling different fields

--- tests/construction_tools.spec.js
+++ tests/construction_tools.spec.js
@@ -21,3 +21,46 @@ test('verify working of concrete calculator', async ({page}) => {
     await expect(page.getByText('12bags · Cement')).toBeVisible();
     await expect(page.getByText('42cu.ft · Aggregate')).toBeVisible();
 })
+
+test('verify working of concrete calculator without filling any fields', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'Try the calculators' }).click();
+    await page.getByRole('link', { name: 'Concrete Calculator Cement,' }).click();
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByText('The length field is required.')).toBeVisible();
+    await expect(page.getByText('The width field is required.')).toBeVisible();
+    await expect(page.getByText('The thickness field is')).toBeVisible();
+})
+
+test('verify working of concrete calculator by only filling length field', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'Try the calculators' }).click();
+    await page.getByRole('link', { name: 'Concrete Calculator Cement,' }).click();
+    await page.getByRole('spinbutton', { name: 'Length' }).click();
+    await page.getByRole('spinbutton', { name: 'Length' }).fill('10');
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByText('The width field is required.')).toBeVisible();
+    await expect(page.getByText('The thickness field is')).toBeVisible();
+})
+
+test('verify working of concrete calculator by only filling width field', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'Try the calculators' }).click();
+    await page.getByRole('link', { name: 'Concrete Calculator Cement,' }).click();
+    await page.getByRole('spinbutton', { name: 'Width' }).click();
+    await page.getByRole('spinbutton', { name: 'Width' }).fill('10');
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByText('The length field is required.')).toBeVisible();
+    await expect(page.getByText('The thickness field is')).toBeVisible();
+})
+
+test('working of concrete calculator by only filling thickness field', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'Try the calculators' }).click();
+    await page.getByRole('link', { name: 'Concrete Calculator Cement,' }).click();
+    await page.getByRole('spinbutton', { name: 'Thickness' }).click();
+    await page.getByRole('spinbutton', { name: 'Thickness' }).fill('0.5');
+    await page.getByRole('button', { name: 'Calculate' }).click();
+    await expect(page.getByText('The length field is required.')).toBeVisible();
+    await expect(page.getByText('The width field is required.')).toBeVisible();
+})
