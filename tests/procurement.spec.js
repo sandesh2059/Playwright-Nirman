08cdc75b62d58845027f889cec28c22a1eefa7c2
Sandesh <sandeshchy2059@gmail.com>
2026-09-14 10:13:07 +0545

    verify request detail page opens by clicking on a request

--- tests/procurement.spec.js
+++ tests/procurement.spec.js
@@ -30,3 +30,20 @@ test('verify working of open request section', async ({page}) => {
 
     await expect(page.getByRole('button', { name: 'Open requests' })).toHaveClass(/bg-surface/);
 });
+
+test('verify request detail page opens by clicking on a request', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'I already have an account' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('cement@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Sign In →' }).click();
+
+    await page.getByRole('link', { name: 'Requests' }).click();
+    await page.getByRole('button', { name: 'Open requests' }).click();
+    await page.getByRole('link', { name: 'Bricks open 1000 bricks' }).click();
+    await expect(page).toHaveURL('https://dev6.yigserver.com:3000/procurement/12');
+
+
+});
