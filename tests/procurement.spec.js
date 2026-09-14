502f9399af2f7bd06349fc2102481af1fdd8286b
Sandesh <sandeshchy2059@gmail.com>
2026-09-14 10:42:34 +0545

    verify posting a request

--- tests/procurement.spec.js
+++ tests/procurement.spec.js
@@ -70,3 +70,30 @@ test('verify sending quotation', async ({page}) => {
     await expect(page.getByRole('dialog', { name: 'Submit Quotation' })).not.toBeVisible();
     
 });
+
+test('verify posting a request', async ({page}) => {
+    await page.goto('https://dev6.yigserver.com:3000/');
+    await page.getByRole('link', { name: 'I already have an account' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('cement@example.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Testuser@12');
+    await page.getByRole('button', { name: 'Sign In →' }).click();
+
+    await page.getByRole('link', { name: 'Requests' }).click();
+    const requestTitle = `Cement open 100 bags of cement ${Date.now()}`;
+    await page.getByRole('link', { name: 'Post' }).click();
+    await page.getByRole('textbox', { name: 'bags of cement' }).click();
+    await page.getByRole('textbox', { name: 'bags of cement' }).fill(requestTitle);
+    await page.getByRole('combobox').first().selectOption('15');
+    await page.locator('input[type="date"]').click();
+    await page.locator('input[type="date"]').fill('2026-09-30');
+    await page.getByRole('textbox', { name: 'e.g. OPC Cement' }).click();
+    await page.getByRole('textbox', { name: 'e.g. OPC Cement' }).fill('cement');
+    await page.getByRole('spinbutton', { name: 'Quantity' }).click();
+    await page.getByRole('spinbutton', { name: 'Quantity' }).fill('100');
+    await page.getByRole('button', { name: 'Post Request' }).click();
+    await page.getByRole('link', { name: 'Back' }).click();
+    await page.getByRole('button', { name: 'My requests' }).click();
+    await expect(page.getByRole('link', { name: requestTitle })).toBeVisible();
+});
